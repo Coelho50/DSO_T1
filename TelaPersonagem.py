955f9833f67cc781f8b951f2f3905d1558f9e4e4
Alec Coelho <aleccoelho50@gmail.com>
2024-10-19 15:57:33 -0300

    Codigo do metodo le_opcao escrito com alguns ajustes ainda a serem feitos

--- TelaPersonagem.py
+++ TelaPersonagem.py
@@ -4,12 +4,23 @@ class TelaPersonagem(AbstractTela):
 	def __init__(self, controlador_personagem):
 		self.__controlador_personagem = controlador_personagem
 
-	def mostra_menu():
+	def mostra_menu(self):
 		print("------- PERSONAGENS -------")
 		print("1 - Criar novo personagem")
 		print("2 - Excluir personagem")
 		print("3 - Listar personagens")
 		print("4 - Retornar ao menu principal")
+		print()
 
-	def le_opcao():
-		pass
+	#deve ser passado para a funcao o tamanho da lista de opcoes a serem escolhidas
+	def le_opcao(self, opcoes:int = 0):
+		if opcoes == 0:	#---------------------------------> perguntar ao professor
+			raise Exception("nao foram passadas as opcoes do usuario")  #-----> criar uma nova excecao?
+		else:
+			while True:
+				try:
+					opcao = int(input("Escolha uma opção: "))
+				except ValueError:
+					print("Ocorreu um erro, selecione a opção com um número inteiro")
+				else:
+					return opcao  #------------>ainda aceitando opcao > opcoes
--- test.py
+++ test.py
@@ -9,6 +9,8 @@ from TelaPersonagem import TelaPersonagem
 arquivo usado APENAS PARA TESTES, DEVE SER EXCLUIDO ANTES DA VERSAO FINAL
 '''
 
+
+#----------------- CONTROLADOR DE PERSONAGEM -----------------
 char_control = ControladorPersonagem()
 
 ''' 
@@ -16,18 +18,26 @@ char_control.cria_personagem("Healer", "Shadowheart", 20.0, "Flail", 0.0, 12.0, 
 char_control.cria_personagem("Healer", "Shadowheart", 20.0, "Flail", 0.0, 12.0, 200.2)
 char_control.remove_personagem("Shadowheart")
 '''
-char_control.cria_personagem("Healer", "Danu", 19.0, "Cajado de gelo", 1.0, 0.0, 160.2)
-char_control.cria_personagem("Healer", "Shadowheart", 20.0, "Flail", 0.0, 12.0, 200.2)
-char_control.cria_personagem("Mago", "Gale", 14.0, "Grimorio", 5.0, 300.0, 0.0)
-char_control.cria_personagem("Guerreiro", "Karlach", 28.0, "Machado infernal", 340.55, 0.0, 0.0)
-char_control.cria_personagem("Guerreiro", "Karlach", 28.0, "Machado infernal", 340.55, 0.0, 0.0)
-char_control.cria_personagem("Healer", "Demeter", 30.0, "Pluma dos deuses", 20.0, 0.0, 400.0)
-print()
-char_control.lista_personagens("Healer")
-print()
-char_control.lista_personagens("healer")
-print()
-char_control.lista_personagens("Guerreiro")
-print()
-char_control.lista_personagens("Mago")
+#char_control.cria_personagem("Healer", "Danu", 19.0, "Cajado de gelo", 1.0, 0.0, 160.2)
+#char_control.cria_personagem("Healer", "Shadowheart", 20.0, "Flail", 0.0, 12.0, 200.2)
+#char_control.cria_personagem("Mago", "Gale", 14.0, "Grimorio", 5.0, 300.0, 0.0)
+#char_control.cria_personagem("Guerreiro", "Karlach", 28.0, "Machado infernal", 340.55, 0.0, 0.0)
+#char_control.cria_personagem("Guerreiro", "Karlach", 28.0, "Machado infernal", 340.55, 0.0, 0.0)
+#char_control.cria_personagem("Healer", "Demeter", 30.0, "Pluma dos deuses", 20.0, 0.0, 400.0)
+#print()
+#char_control.lista_personagens("Healer")
+#print()
+#char_control.lista_personagens("healer")
+#print()
+#char_control.lista_personagens("Guerreiro")
+#print()
+#char_control.lista_personagens("Mago")
 
+
+
+
+#--------------------- TELA PERSONAGEM ------------------------
+
+tela_personagem = TelaPersonagem(char_control)
+
+print(tela_personagem.le_opcao(4))
