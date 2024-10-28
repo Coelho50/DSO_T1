aa2c8724c40270680baa568913dbac8bab6f71f1
brunoqcastro <2001bruno9@gmail.com>
2024-10-28 11:00:08 -0300

    add e remover party

--- Codigo Fonte/controladores/ControladorParty.py
+++ Codigo Fonte/controladores/ControladorParty.py
@@ -1,5 +1,6 @@
 from entidades.Jogador import Jogador
 from entidades.Party import Party
+from excecoes import GrupoCheioException
 from telas.TelaParty import TelaParty
 
 #Controle responsavel pelas parties do jogador. Instancia a tela de personagens a ser utilizada pelo usuario
@@ -7,4 +8,21 @@ class ControladorParty():
 	def __init__(self, jogador: Jogador):
 		self.__tela_party = TelaParty(self)
 		self.__jogador = jogador
+	
+	def cria_party(self, nome: str, personagens: list):
+		try:
+			self.__jogador.add_parties(Party(personagens))
+		except ValueError:
+			pass
+		except GrupoCheioException:
+			pass
+
+	def remover_party(self, nome:str):
+		try:
+			self.__jogador.remove_party(nome)
+		except:
+			pass
+		
+	def listar_parties(self):
+		return self.__jogador.parties
 	
