3f096d6e2d23aead5d00e7ab1b4e11427dc2941d
Alec Coelho <aleccoelho50@gmail.com>
2024-11-04 10:23:08 -0300

    adicionando arquivo main e alterando a descrição do projeto

--- Codigo Fonte/test.py
+++ Codigo Fonte/test.py
@@ -29,59 +29,17 @@ main_control._ControladorPrincipal__controlador_personagem._ControladorPersonage
 main_control._ControladorPrincipal__controlador_personagem._ControladorPersonagem__personagens_cadastrados.append(Mago("Cyrax", 20.0, "axe", 30.2, 40.4, 50.5))
 
 joao = Healer("joao",1.0,"",1.0,1.0,1.0)
-matheus = Healer("joao",1.0,"",1.0,1.0,1.0)
-marcos = Healer("joao",1.0,"",1.0,1.0,1.0)
-lucas = Healer("joao",1.0,"",1.0,1.0,1.0)
-main_control.jogadores_cadastrados[1].add_party(Party("Curandeiros",joao,matheus,marcos,lucas))
-main_control.jogadores_cadastrados[0].add_party(Party("Magos de ataque",joao,matheus,marcos,lucas))
+matheus = Healer("matheus",1.0,"",1.0,1.0,1.0)
+marcos = Healer("marcos",1.0,"",1.0,1.0,1.0)
+lucas = Healer("lucas",1.0,"",1.0,1.0,1.0)
 
 main_control._ControladorPrincipal__jogadores_cadastrados[0]._Jogador__vitorias = 32
 main_control._ControladorPrincipal__jogadores_cadastrados[1]._Jogador__vitorias = 63
 main_control._ControladorPrincipal__jogadores_cadastrados[2]._Jogador__vitorias = 54
 main_control._ControladorPrincipal__jogadores_cadastrados[3]._Jogador__vitorias = 12
 main_control._ControladorPrincipal__jogadores_cadastrados[4]._Jogador__vitorias = 60
+
+main_control.jogadores_cadastrados[1].add_party(Party("Curandeiros",joao,matheus,marcos,lucas))
+main_control.jogadores_cadastrados[0].add_party(Party("Magos de ataque",joao,matheus,marcos,lucas))
+
 main_control.abrir_sistema()
-
-
-#----------------- CONTROLADOR DE PERSONAGEM -----------------
-
-#char_control = ControladorPersonagem()
-#
-# 
-#char_control.cria_personagem("Healer", "Shadowheart", 20.0, "Flail", 0.0, 12.0, 200.2)
-#char_control.cria_personagem("Healer", "Shadowheart", 20.0, "Flail", 0.0, 12.0, 200.2)
-#char_control.remove_personagem("Shadowheart")
-#'''
-#char_control.cria_personagem("Healer", "Danu", 19.0, "Cajado de gelo", 1.0, 0.0, 160.2)
-#char_control.cria_personagem("Healer", "Shadowheart", 20.0, "Flail", 0.0, 12.0, 200.2)
-#char_control.cria_personagem("Mago", "Gale", 14.0, "Grimorio", 5.0, 300.0, 0.0)
-#char_control.cria_personagem("Guerreiro", "Karlach", 28.0, "Machado infernal", 340.55, 0.0, 0.0)
-#char_control.cria_personagem("Guerreiro", "Karlach", 28.0, "Machado infernal", 340.55, 0.0, 0.0)
-#char_control.cria_personagem("Healer", "Demeter", 30.0, "Pluma dos deuses", 20.0, 0.0, 400.0)
-#'''
-#print()
-#char_control.lista_personagens("Healer")
-#print()
-#char_control.lista_personagens("healer")
-#print()
-#char_control.lista_personagens("Guerreiro")
-#print()
-#char_control.lista_personagens("Mago")
-#
-#
-#'''
-
-#--------------------- TELA PERSONAGEM ------------------------
-'''
-tela_personagem = TelaPersonagem(char_control)
-
-print(tela_personagem.le_opcao(4))
-'''
-
-#def pegar_dados(msg, tipo):
-#	try:
-#		dado = tipo(input(msg))
-#	except ValueError:
-#		print(f'Dado passado deve ser do tipo {tipo}')
-#
-#pegar_dados("bla", str)
