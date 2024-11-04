d60f19c4a39c99ed21843ac6a965eeda47838227
brunoqcastro <2001bruno9@gmail.com>
2024-11-04 10:10:59 -0300

    Ajustes no controlador de batalha
    Corrigido bug na função de remover batalha e criação de batalha não pode mais ser contra sigo mesma

--- Codigo Fonte/controladores/ControladorBatalha.py
+++ Codigo Fonte/controladores/ControladorBatalha.py
@@ -14,26 +14,30 @@ class ControladorBatalha():
 		self.__jogadores = controlador_principal.jogadores_cadastrados
 
 	def cria_batalha(self):
-		self.__tela_batalha.mostra_mensagem("--------- CRIAÇÃO DE BATALHA ---------")
-		jogador2 = self.verificador(self.__jogadores,"Quem é o jogador adversário?(digite '0' para cancelar)",JogadorNotFoundException)
-		if jogador2 == None:
-			return None
-		party1 = self.verificador(self.__jogador1.parties,"Qual a sua party?(digite '0' para cancelar)",PartyNotFoundException)
-		if party1 == None:
-			return None
-		party2 = self.verificador(jogador2.parties,"Qual a party do adversário?(digite '0' para cancelar)",PartyNotFoundException)
-		if party2 == None:
-			return None
-		vencedor = self.verificador([self.__jogador1, jogador2],"Quem ganhou a batalha?", JogadorNotFoundException)
-		if vencedor == None:
-			return None
-		b = Batalha(self.__jogador1, jogador2, party1, party2, vencedor)
-		self.__jogador1.add_batalha(b)
-		jogador2.add_batalha(b)
-		
-		self.__tela_batalha.mostra_mensagem("batalha criada!")
+			self.__tela_batalha.mostra_mensagem("--------- CRIAÇÃO DE BATALHA ---------")
+			while True:
+				jogador2 = self.verificador(self.__jogadores,"Quem é o jogador adversário?(digite '0' para cancelar)",JogadorNotFoundException)
+				if jogador2 == None:
+					return None
+				elif jogador2 == self.__jogador1:
+					self.__tela_batalha.mostra_mensagem("Jogador não pode batalhar contra sigo mesmo!")
+				else:
+					break
+			party1 = self.verificador(self.__jogador1.parties,"Qual a sua party?(digite '0' para cancelar)",PartyNotFoundException)
+			if party1 == None:
+				return None
+			party2 = self.verificador(jogador2.parties,"Qual a party do adversário?(digite '0' para cancelar)",PartyNotFoundException)
+			if party2 == None:
+				return None
+			vencedor = self.verificador([self.__jogador1, jogador2],"Quem ganhou a batalha?", JogadorNotFoundException)
+			if vencedor == None:
+				return None
+			b = Batalha(self.__jogador1, jogador2, party1, party2, vencedor)
+			self.__jogador1.add_batalha(b)
+			jogador2.add_batalha(b)
+			self.__tela_batalha.mostra_mensagem("batalha criada!")
 
-	def remove_batalha(self, nome:str):
+	def remove_batalha(self):
 		while True:
 			try:
 				self.__tela_batalha.mostra_mensagem("Qual batalha deseja remover?(digite '0' para cancelar)")
@@ -47,12 +51,12 @@ class ControladorBatalha():
 					j2 = self.__jogador1.batalhas[n-1].jogador_2 # Se remover o jogador logado primeiro não será possível remover o segundo
 					j1.remove_batalha(b)
 					j2.remove_batalha(b)
+					self.__tela_batalha.mostra_mensagem("Batalha removida!")
+					return None
 				else:
 					raise BatalhaNotFoundException
 			except BatalhaNotFoundException as e:
 				self.__tela_batalha.mostra_mensagem(e)
-
-
 		
 	def lista_batalhas(self):
 		self.__tela_batalha.mostra_mensagem("------------ BATALHAS DO JOGADOR ------------")
@@ -61,9 +65,9 @@ class ControladorBatalha():
 			self.__tela_batalha.mostra_mensagem(f"{c}° Batalha")
 			self.__tela_batalha.mostra_mensagem(f"{b.jogador_1.nome}({b.party_1.nome}) X {b.jogador_2.nome}({b.party_2.nome})")
 			self.__tela_batalha.mostra_mensagem(f"Vencedor: {b.vencedor.nome}")
-			self.__tela_batalha.mostra_mensagem("------------------------------")
-		self.__tela_batalha.mostra_mensagem()
-		
+			self.__tela_batalha.mostra_mensagem("-----------------------------------")
+		self.__tela_batalha.mostra_mensagem(f"Número total de batalhas: {len(self.__jogador1.batalhas)}")
+		self.__tela_batalha.mostra_mensagem(f"Vitórias: {self.__jogador1.vitorias}")
 
 	def abrir_menu(self):
 		self.__jogador1 = self.__controlador_principal.jogador_logado
